c316dac6172f83c1d57a952837eebef232388fc9
ghuysu <daogiahuysu@gmail.com>
2025-07-09 13:43:50 +0700

    init

---
+++ main.js
@@ -0,0 +1,16 @@
+const express = require('express');
+
+const app = express();
+const port = 3000;
+
+app.get('/helloworld', (req, res) => {
+    res.send('Hello, world!\n');
+});
+
+app.get('/', (req, res) => {
+    res.send(`Server is running at http://localhost:${port}\n`);
+});
+
+app.listen(port, () => {
+    console.log(`Server is running at http://localhost:${port}`);
+});
